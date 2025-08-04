0dec9f7e5ea5429932b03356aa23dbd5c983fd46
pickmycourse <tech@pickmycourse.io>
2025-08-04 16:16:39 +0530

    sd

--- client/src/pages/dashboard/MyStudyGroupsPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MyStudyGroupsPage = () => {
-    return <h1>My Study Groups</h1>;
-};
-
-export default MyStudyGroupsPage;
--- client/src/pages/dashboard/ProfilePage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ProfilePage = () => {
-    return <h1>Profile</h1>;
-};
-
-export default ProfilePage;
